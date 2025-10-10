c4cb69323c2d92892d4fc309092915d756560ccd
Shuvo <77.shuvo.joy@gmail.com>
2025-10-11 01:52:52 +0600

    login page redesigned

--- frontend/src/pages/Welcome.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-export default function Welcome() {
-  // Redirect to login if accessing welcome directly
-  return <Navigate to="/login" replace />;
-}
